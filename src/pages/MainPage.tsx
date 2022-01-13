a6494fe6ef86cfeae93fb1ba131d482af0759025
amongDOHEE <dohee@sandboxnetwork.net>
2022-01-13 15:27:41 +0900

    modify: push fix code

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { CheckToken } from "../api/authLogic";
+import { CheckToken } from "../api/auth";
 import LoginButton from "../components/LoginButton";
 
 const MainPage: React.FC = (): JSX.Element => {
@@ -26,7 +26,7 @@ const MainPage: React.FC = (): JSX.Element => {
 
   return (
     <div className="loginPage">
-      <img className="logo-image" src="https://platum.kr/wp-content/uploads/2018/09/aaa.png"/>
+      <img className="logo-image" alt="sandbox-logo" src="https://platum.kr/wp-content/uploads/2018/09/aaa.png"/>
       <LoginButton/>   
     </div>
   );
